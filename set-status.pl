4f1c071fc7e95f1113a3dc600b36937a7566bc97
ben <livingood@gmail.com>
2009-05-17 21:19:56 -0600

    initial work on a config file

--- set-status.pl
+++ set-status.pl
@@ -2,10 +2,23 @@
  
 use Net::Twitter;
 use Data::Dumper;
- 
+use YAML::XS;
+
+open FP, "config.yml";
+
+while (<FP>){
+	$config = $config.$_;
+}
+close FP;
+
+my $options = Load $config;
+
+if $options->{"username"} && $options->{"password"} {
+	 
 $phrase = <>;
-my $twit = Net::Twitter->new(username=>'username', password=>'password');
+my $twit = Net::Twitter->new(username=>$options->("username"), password=>$options->{"password"});
  
 $result = $twit->update($phrase);
  
 print Dumper $result;
+}
